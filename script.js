e82549d2d43cad622838951a3b5d005e08767de4
Md Juman <mdjuman.developer@gmail.com>
2024-11-09 17:21:02 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementsByClassName('button')[0];
+let bulb = document.getElementsByClassName('light')[0];
+let lightImg = document.getElementsByClassName('light-img')[0];
+var c = 0;
+btn.addEventListener('click', function () {
+  if (c == 0) {
+    bulb.style.display = 'block';
+    lightImg.src = './lighton.png';
+    c = 1;
+  } else {
+    bulb.style.display = 'none';
+    lightImg.src = './lightoff.png';
+    c = 0;
+  }
+});
